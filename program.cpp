a744d7ee68acb4da6a62973f2188920b82942c14
Igor <ihor.zhelezniak@mths.ca>
2021-10-27 00:18:36 +0000

    aded program

---
+++ program.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Igor All rights reserved
+//
+// Created by: Igor
+// Created on: Oct 2021
+// This program calculates the area and perimeter of a rectangle
+//    with dimensions 5cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
+    std::cout << std::endl;
+}
